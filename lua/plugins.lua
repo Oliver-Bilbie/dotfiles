190a895b2242b4a210cf43db1643fa85985c195f
Oliver Bilbie <oliver.bilbie@edfenergy.com>
2024-04-13 10:33:40 +0100

    Removed nvim-notify

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -158,15 +158,6 @@ packer.startup({
 		-- Highlight URLs inside vim
 		use({ "itchyny/vim-highlighturl", event = "VimEnter" })
 
-		-- notification plugin
-		-- use({
-		-- 	"rcarriga/nvim-notify",
-		-- 	event = "VimEnter",
-		-- 	config = function()
-		-- 		require("config.nvim-notify")
-		-- 	end,
-		-- })
-
 		-- lsp loading spinner
 		use({
 			"j-hui/fidget.nvim",
